cab3da12531062f07a18c7647a5eaa34ea17170f
Taras Tataryn <taras.tataryn.ttm@gmail.com>
2019-01-23 19:39:31 +0200

    Register store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+
+import { NgMatModule } from './ng-mat.module';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgMatModule } from './ng-mat.module';
 import { CarComponent } from './car/car.component';
 import { CarFormComponent } from './car-form/car-form.component';
 import { CarListComponent } from './car-list/car-list.component';
+import { carsReducer } from './@ngrx/cars.reducer';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarComponent,
     CarFormComponent,
-    CarListComponent
+    CarListComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    NgMatModule
+    NgMatModule,
+    StoreModule.forRoot({carsReducer: carsReducer}),
   ],
   providers: [],
   bootstrap: [AppComponent]
